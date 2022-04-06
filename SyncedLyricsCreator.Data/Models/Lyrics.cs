511b078c4145bb8e877b1b41c42f928dbbfca4d4
UrbanCMC <UrbanCMC@web.de>
2022-04-06 20:19:08 +0200

    Add lyrics converter

---
+++ SyncedLyricsCreator.Data/Models/Lyrics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SyncedLyricsCreator.Data.Models
+{
+    /// <summary>
+    /// Represents the synchronized lyrics for a song
+    /// </summary>
+    public class Lyrics
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Lyrics"/> class
+        /// </summary>
+        public Lyrics() => Lines = new List<LyricsLine>();
+
+        /// <summary>
+        /// Gets or sets a list of lines, each of which is timestamped
+        /// </summary>
+        public List<LyricsLine> Lines { get; set; }
+
+        /// <summary>
+        /// Represents a single lyrics line
+        /// </summary>
+        public struct LyricsLine
+        {
+            /// <summary>
+            /// The timestamp indicating when the line should be displayed
+            /// </summary>
+            public TimeSpan Timestamp;
+
+            /// <summary>
+            /// The text to display
+            /// </summary>
+            public string Text;
+        }
+    }
+}
---
+++ SyncedLyricsCreator.Data/Services/LyricsConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using SyncedLyricsCreator.Data.Models;
+using TagLib.Id3v2;
+
+namespace SyncedLyricsCreator.Data.Services
+{
+    /// <summary>
+    /// Converts lyrics between the internally used <see cref="Lyrics"/> and the ID3 <see cref="SynchronisedLyricsFrame"/>
+    /// </summary>
+    public static class LyricsConverter
+    {
+        /// <summary>
+        /// Loads a lyrics object from an ID3 tag object
+        /// </summary>
+        /// <param name="tag">The ID3 tag object to load from</param>
+        /// <returns>A <see cref="Lyrics"/> object containing the loaded lines</returns>
+        /// <remarks>
+        /// For now, only lyrics frames for english text will be read from
+        /// </remarks>
+        public static Lyrics LoadFromTag(Tag tag)
+        {
+            var lyrics = new Lyrics();
+            var lyricsFrame = SynchronisedLyricsFrame.Get(tag, "", "eng", SynchedTextType.Lyrics, true);
+
+            lyrics.Lines = lyricsFrame.Text.Select(s => new Lyrics.LyricsLine
+            {
+                Timestamp = TimeSpan.FromMilliseconds(s.Time),
+                Text = s.Text,
+            }).ToList();
+
+            return lyrics;
+        }
+
+        /// <summary>
+        /// Writes lyrics to an ID3 tag object, overwriting existing ones if present.
+        /// </summary>
+        /// <param name="lyrics">The synchronized lyrics to save</param>
+        /// <returns>The created ID3 frame containing the synchronized lyrics</returns>
+        /// <remarks>
+        /// For now we always use english as the target language for our frame
+        /// </remarks>
+        public static SynchronisedLyricsFrame SaveToFrame(Lyrics lyrics)
+            => new("", "eng", SynchedTextType.Lyrics)
+            {
+                Text = lyrics.Lines
+                    .Select(s => new SynchedText((long)s.Timestamp.TotalMilliseconds, s.Text))
+                    .ToArray(),
+            };
+    }
+}
