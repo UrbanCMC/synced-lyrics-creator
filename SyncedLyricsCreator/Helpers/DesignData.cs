9e6600a2e77a7808930dd46d9e6ef20de540df42
UrbanCMC <UrbanCMC@web.de>
2022-08-15 12:18:06 +0200

    Add design-time bindings

---
+++ SyncedLyricsCreator/Helpers/DesignData.cs
@@ -0,0 +1,29 @@
+﻿using SyncedLyricsCreator.ViewModels;
+
+namespace SyncedLyricsCreator.Helpers
+{
+    /// <summary>
+    /// Provides design-time data to make the visual design tools more useful
+    /// </summary>
+    public static class DesignData
+    {
+        /// <summary>
+        /// Gets a design-time <see cref="LyricsEditorViewModel"/>
+        /// </summary>
+        public static LyricsEditorViewModel LyricsEditorViewModel { get; } = new();
+
+        /// <summary>
+        /// Gets a design-time <see cref="PlayerViewModel"/>
+        /// </summary>
+        public static PlayerViewModel PlayerViewModel { get; } = new();
+
+        /// <summary>
+        /// Gets a design-time <see cref="MainWindowViewModel"/>
+        /// </summary>
+        public static MainWindowViewModel MainWindowViewModel { get; } = new()
+        {
+            LyricsEditorViewModel = LyricsEditorViewModel,
+            PlayerViewModel = PlayerViewModel,
+        };
+    }
+}
